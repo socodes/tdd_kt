19b0f283b951f49be7b6dd4abaabac8e94b0bfc1
Muhammed Didin <mdidin80@gmail.com>
2022-02-26 12:24:09 +0300

    Plus, minus and GCD tests are added.
    -Comments will be added.
    -CI integration will be added

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,11 +1,30 @@
 class Calculator {
+    //parse function parses the input. Correct format would be:
+    //a op b
+    //a -> Int op-> String b-> Int
     fun parse(s: String): Int {
         val (a,op,b) = s.split(" ")
         return when(op) {
             "*" -> a.toInt() * b.toInt()
             "/" -> a.toInt() / b.toInt()
+            "+" -> a.toInt() + b.toInt()
+            "-" -> a.toInt() - b.toInt()
+            "g" -> gcd(a.toInt(),b.toInt())
             else -> throw IllegalArgumentException("Invalid Operator")
         }
+    }
+    //gcd function calculates Greatest Common Divisor
+    //takes two inputs and returns the gcd of them in Int.
+    fun gcd(n1:Int, n2:Int): Int {
+        var gcd = 1
+        var i = 1
+        while (i <= n1 && i <= n2) {
+            // Checks if i is factor of both integers
+            if (n1 % i == 0 && n2 % i == 0)
+                gcd = i
+            ++i
+        }
+        return gcd
     }
 
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,4 +14,22 @@ class CalculatorTest {
         val result = calculator.parse("2 / 2")
         assertEquals(1,result)
     }
+    @Test
+    fun testPlus2By2(){
+        val calculator = Calculator()
+        val result = calculator.parse("2 + 2")
+        assertEquals(4,result)
+    }
+    @Test
+    fun testMinus2By2(){
+        val calculator = Calculator()
+        val result = calculator.parse("2 - 2")
+        assertEquals(0,result)
+    }
+    @Test
+    fun testGCD81and153(){
+        val calculator = Calculator()
+        val result = calculator.parse("81 g 153")
+        assertEquals(9,result)
+    }
 }
