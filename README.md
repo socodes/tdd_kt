fb0630afda08041623a38fc8e4c7de2541f730ec
Muhammed Didin <mdidin80@gmail.com>
2022-02-26 12:36:35 +0300

    Readme and code comments added

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# tdd_kt
+# Test Driven Development in Kotlin
+
+JUnit 4 and kotlin.test libraries are used.
+
+Tests cover four main operations in a calculator and greatest common divisor operations.
+
+###Results:
+
+    testGCD81and153:
+    expected: 9
+    result: 9
+
+    testMinus2By2:
+    expected: 0
+    result: 0
+
+    testPlus2By2:
+    expected: 4
+    result: 4
+
+    testMultiply2By2:
+    expected: 4
+    result: 4
+
+    testDivide2By2:
+    expected: 1
+    result: 1
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,30 +3,35 @@ import org.junit.Test
 
 class CalculatorTest {
     @Test
+    //testing multiplying operation.
     fun testMultiply2By2(){
         val calculator = Calculator()
         val result = calculator.parse("2 * 2")
         assertEquals(4,result)
     }
     @Test
+    //testing divide operation
     fun testDivide2By2(){
         val calculator = Calculator()
         val result = calculator.parse("2 / 2")
         assertEquals(1,result)
     }
     @Test
+    //testing adding operation
     fun testPlus2By2(){
         val calculator = Calculator()
         val result = calculator.parse("2 + 2")
         assertEquals(4,result)
     }
     @Test
+    //testing substraction operation
     fun testMinus2By2(){
         val calculator = Calculator()
         val result = calculator.parse("2 - 2")
         assertEquals(0,result)
     }
     @Test
+    //testing greatest common divisor operation
     fun testGCD81and153(){
         val calculator = Calculator()
         val result = calculator.parse("81 g 153")
